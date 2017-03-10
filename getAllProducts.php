69ebde0f520fb9336a5052b201a86b520ed21d21
Elías Pujol Rodríguez <1715110467@utec-tgo.edu.mx>
2017-03-10 09:37:52 -0600

    Update getAllProducts.php

--- getAllProducts.php
+++ getAllProducts.php
@@ -4,7 +4,7 @@
 		$db_user = "ff33i0703xc9naqx";
 		$db_password = "xo5fmwejcfqlc21b";
 		    
-		$connection = mysql_connect($db_host, $db_user, $db_password) or die("Connection Error: " .mysql_error());
+		$connection = mysql_connect(u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com, g4hh70qbo9s0xg8y,xo5fmwejcfqlc21b ) or die("Connection Error: " .mysql_error());
 		    
 		mysql_select_db($db_name) or die("Error al seleccionar la base de datos:".mysql_error());
 		@mysql_query("SET NAMES 'utf8'");
